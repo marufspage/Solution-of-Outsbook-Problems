24f150e323d1788f0396e5cab43b799e9c32bbb6
marufurrahman <marufurrahman1997@gmail.com>
2018-11-06 19:49:20 +0600

    Accepted: Can You Type It?

---
+++ Solutions/Outsbook-100.cpp
@@ -0,0 +1,11 @@
+// Problem link: http://school.outsbook.com/problems/problemdetails/100
+// Code written by Marufur Rahman
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Outsbook\n";
+
+	return 0;
+}
